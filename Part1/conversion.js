4e24cf489dcdf8bed9a599e3cb1ed6128c707dbc
Devotion <letsmaildipak@gmail.com>
2024-01-13 20:26:11 +0530

    learned about conversion, operations, assignments etc

---
+++ Part1/conversion.js
@@ -0,0 +1,9 @@
+let id = "123";
+
+let NewId = Number(id);
+console.log(NewId);
+console.log(typeof NewId);
+
+/* Same way you can convert number to string by using String.
+remember that, First letter must be capital.
+Null, Undefined, Object etc. */
