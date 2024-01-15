285ac45a2e1b58a78a185327da5dd975f660f68d
Devotion <letsmaildipak@gmail.com>
2024-01-15 20:45:38 +0530

    learned replacing a sub string, spliting itho array, and finding which character is at given index

--- Part1/string.js
+++ Part1/string.js
@@ -13,6 +13,7 @@ console.log(`${name} ${id}`);
 const str = new String("Dipak");
 
 const newString = "    Dipak   ";
+const str2 = "Jadhav";
 
 console.log(newString);
 console.log(`${newString.trim()}`);
@@ -20,3 +21,7 @@ console.log(`${newString.trim()}`);
 const url = "https://Dipak.com/dipak%20jadhav";
 
 console.log(url.replace("%20", "-"));
+
+console.log(newString.split(""));
+
+console.log(str2.charAt(2));
