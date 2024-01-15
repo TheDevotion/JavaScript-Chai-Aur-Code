57531fc71f6358d1a1ee477162417f8563c6c53a
Devotion <letsmaildipak@gmail.com>
2024-01-15 20:40:58 +0530

    learned about slice, trim replace, includes

--- Part1/string.js
+++ Part1/string.js
@@ -9,3 +9,14 @@ console.log(name + id);
 // You can use this instead:
 
 console.log(`${name} ${id}`);
+
+const str = new String("Dipak");
+
+const newString = "    Dipak   ";
+
+console.log(newString);
+console.log(`${newString.trim()}`);
+
+const url = "https://Dipak.com/dipak%20jadhav";
+
+console.log(url.replace("%20", "-"));
